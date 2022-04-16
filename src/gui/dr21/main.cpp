7852636ffd0ffa42a45155935d423e2b3e1ec704
Sopa02 <kissopa02@gmail.com>
2022-04-16 12:14:38 +0200

    dr21 in progress

--- src/gui/dr21/main.cpp
+++ src/gui/dr21/main.cpp
@@ -5,7 +5,8 @@
 #include <list>
 #include <algorithm>
 
-struct Item{
+struct Item
+{
     std::string name;
     int iid;
     double value;
@@ -18,7 +19,7 @@ struct Item{
         double getValue(){return value;}
 };
 std::ostream& operator<<(std::ostream& os, Item& i){
-    os<<i.getName()<<';'<<i.getID()<<';'<<i.getValue();
+    os<<i.getName()<<"; "<<i.getID()<<"; "<<i.getValue();
     return os;
 }
 std::istream& operator>>(std::istream& is, Item& i){
@@ -38,9 +39,25 @@ std::istream& operator>>(std::istream& is, Item& i){
     i = Item{n,id,v};
     return is;
 }
-
-
-void readFromFile(std::string filename, std::vector<Item>& v){
+template<typename T> void print(T vec, std::string cap){
+    
+    std::cout<<"\n\n"<<cap<<"\n\n";
+    for (auto& i : vec)
+    {
+        std::cout<<i<<'\n';
+    }
+    
+}
+template<typename Iterator, typename Predicate>
+Iterator myfind(Iterator first, Iterator last, Predicate pred){
+    while (first != last && !pred(*first))
+    {
+        first++;
+    }
+    return first;
+}
+template<typename T>
+void readFromFile(std::string filename, T& v){
     std::ifstream ins {filename};
     for (Item i; ins>>i;)
     {
@@ -48,41 +65,86 @@ void readFromFile(std::string filename, std::vector<Item>& v){
     }
 
 }
-void readFromFile(std::string filename, std::list<Item>& l){
-    std::ifstream ins {filename};
-    for (Item i; ins>>i;)
-    {
-        l.push_back(i);
-    }
-    
-}
+
+
+struct Compare_by_name
+{
+    bool operator()(const Item& a, const Item& b) const {return a.name<b.name;}
+};
+struct Compare_by_iid
+{
+    bool operator()(const Item& a, const Item& b) const {return a.iid<b.iid;}
+};
+struct Compare_by_value
+{
+    bool operator()(const Item& a, const Item& b) const {return a.value>b.value;}
+};
+struct Name_eq
+{
+    std::string n;
+    public:
+        Name_eq(std::string name){n=name;}
+        bool operator()(const Item& a) const {return n == a.name;}
+};
+struct Iid_eq
+{
+    int id;
+    public:
+        Iid_eq(int iid){id=iid;}
+        bool operator()(const Item& a) const {return id == a.iid;}
+};
+
+
 
 int main(){
 
     std::string filename = "file.txt";
 
     //---Vector
-    std::cout<<"Vector:\n\n";
 
     std::vector<Item> vi;
 
     readFromFile(filename,vi);
     
-    for (Item i : vi)
-    {
-        std::cout<<i<<"\n";
-    }
+    print(vi,"Vecor:");
+
+    std::sort(vi.begin(),vi.end(), Compare_by_name()); // sort by name
+    std::sort(vi.begin(),vi.end(), Compare_by_iid()); // sort by iid
+    std::sort(vi.begin(),vi.end(), Compare_by_value()); // sort by value
     
+    print(vi,"Vector after sort by value:");
+
+    vi.push_back(Item{"horse shoe",99,12.34});
+    vi.push_back(Item{"Canon S400",9988,499.95});
+    print(vi,"Inserting two:");
+    vi.erase(myfind(vi.begin(),vi.end(),Name_eq{"soap"}));
+    vi.erase(myfind(vi.begin(),vi.end(),Name_eq{"orange peel"}));
+    print(vi,"Erasing soap and orange peel:");
+    vi.erase(myfind(vi.begin(),vi.end(),Iid_eq{1}));
+    vi.erase(myfind(vi.begin(),vi.end(),Iid_eq{3}));
+    print(vi,"Erasing items with the id of 1 and 3: ");
+
     //---List
-    std::cout<<"\n\nList:\n\n";
 
     std::list<Item> li;
     readFromFile(filename,li);
-    for (Item i : li)
-    {
-        std::cout<<i<<"\n";
-    }
+    print(li,"List:");
+    li.sort(Compare_by_name());
+    li.sort(Compare_by_iid());
+    li.sort(Compare_by_value());
 
-    //---Map
+    print(li,"List after sort by value:");
+
+    li.push_back(Item{"horse shoe",99,12.34});
+    li.push_back(Item{"Canon S400",9988,499.95});
+    print(li,"Inserting two:");
+    li.erase(myfind(li.begin(),li.end(),Name_eq{"soap"}));
+    li.erase(myfind(li.begin(),li.end(),Name_eq{"orange peel"}));
+    print(li,"Erasing soap and orange peel:");
+    li.erase(myfind(li.begin(),li.end(),Iid_eq{1}));
+    li.erase(myfind(li.begin(),li.end(),Iid_eq{3}));
+    print(li,"Erasing items with the id of 1 and 3: ");
+
+  //---Map
 
 }
